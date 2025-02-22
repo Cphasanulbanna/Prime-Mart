171690266566ec5a74a7fccbcb8857c92eb7f536
Cphasanulbanna <cphasanulbanna@gmail.com>
2025-02-22 12:01:15 +0530

    port update

--- server/routes/product.routes.js
+++ server/routes/product.routes.js
@@ -1,7 +1,5 @@
 import express from "express";
-import mongoose from "mongoose";
 
-import Product from "../models/product.model.js";
 import {
   createProduct,
   deleteProduct,
--- server/server.js
+++ server/server.js
@@ -1,19 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
+
 import { connectDb } from "./config/db.js";
+
 import ProductRoutes from "./routes/product.routes.js";
 
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 app.use(express.json()); //allow to use json data in req body
 app.use("/api/products", ProductRoutes);
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
   connectDb();
-  console.log("server is running http://localhost:5000");
+  console.log(`server is running http://localhost:${PORT}`);
 });
-
-//  WybBsEx0mVWgfYZ5
-
-// mongodb+srv://cphasanulbanna:WybBsEx0mVWgfYZ5@cluster0.1v6hy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
