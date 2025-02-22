add70e1cc6a6a86589710995769e5edf4df0e56a
Cphasanulbanna <cphasanulbanna@gmail.com>
2025-02-22 18:52:05 +0530

    production configurations

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 
 import { connectDb } from "./config/db.js";
 
@@ -9,8 +10,19 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
+
 app.use(express.json()); //allow to use json data in req body
 app.use("/api/products", ProductRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/dist")));
+
+  //render react app if any get requests comes for routes other than express routes
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "/client", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   connectDb();
