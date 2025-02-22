7be082e8fe19b814ef1840ad5e3af86eba40c2e5
Cphasanulbanna <cphasanulbanna@gmail.com>
2025-02-22 14:49:53 +0530

    chakra ui setup

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "@chakra-ui/react";
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <Button>button</Button>
+    </div>
+  );
+};
+
+export default App;
