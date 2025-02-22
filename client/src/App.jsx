2aa2755e39e190a10f2bf08c81962872994f51ec
Cphasanulbanna <cphasanulbanna@gmail.com>
2025-02-22 15:37:43 +0530

    create page ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
-import { Box, Button, useColorModeValue } from "@chakra-ui/react";
-import React from "react";
+import { Box, useColorModeValue } from "@chakra-ui/react";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import CreateProduct from "./pages/CreateProduct";
--- client/src/pages/CreateProduct.jsx
+++ client/src/pages/CreateProduct.jsx
@@ -1,7 +1,67 @@
-import React from "react";
+import {
+  Box,
+  Button,
+  Container,
+  Heading,
+  Input,
+  useColorModeValue,
+  VStack,
+} from "@chakra-ui/react";
+import { useState } from "react";
 
 const CreateProduct = () => {
-  return <div>CreateProduct</div>;
+  const [product, setProduct] = useState({
+    name: "",
+    price: "",
+    image: "",
+  });
+
+  const handleAddProduct = () => {};
+  return (
+    <Container maxW={"container.sm"}>
+      <VStack spacing={8}>
+        <Heading as={"h1"} size={"2xl"} textAlign={"center"} mb={8}>
+          Create new Product
+        </Heading>
+        <Box
+          w={"full"}
+          bg={useColorModeValue("white", "gray.800")}
+          p={6}
+          rounded={"lg"}
+          shadow={"md"}
+        >
+          <VStack spacing={4}>
+            <Input
+              placeholder="Product Name"
+              name="name"
+              value={product.name}
+              onChange={(e) => setProduct({ ...product, name: e.target.value })}
+            />
+            <Input
+              placeholder="Product Price"
+              name="price"
+              value={product.price}
+              type="number"
+              onChange={(e) =>
+                setProduct({ ...product, price: e.target.value })
+              }
+            />
+            <Input
+              placeholder="Product Image URL"
+              name="image"
+              value={product.image}
+              onChange={(e) =>
+                setProduct({ ...product, image: e.target.value })
+              }
+            />
+            <Button colorScheme="blue" onClick={handleAddProduct} w={"full"}>
+              Add Product
+            </Button>
+          </VStack>
+        </Box>
+      </VStack>
+    </Container>
+  );
 };
 
 export default CreateProduct;
